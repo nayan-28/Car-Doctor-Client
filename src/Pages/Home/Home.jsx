56b3f2d54965817412d9d10336ab9af2807f67ca
nayan-28 <nayanmalakar720@gmail.com>
2024-03-01 13:50:41 +0600

    footer and navbar added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>This s Home</h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-
+import Home from "../Pages/Home/Home";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element:
+        element: <Home></Home>,
       },
     ],
   },
