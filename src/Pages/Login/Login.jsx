0a28033102842b72f070068fb2e280f51677032d
nayan-28 <nayanmalakar720@gmail.com>
2024-03-21 21:43:19 +0600

    jwt token added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { useContext } from "react";
+import axios from "axios";
 const Login = () => {
   const { SignIn } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log(location);
   const handleLogin = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -12,8 +16,13 @@ const Login = () => {
 
     SignIn(email, password)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        const user = { email };
+        axios.post("http://localhost:5000/jwt", user).then((res) => {
+          console.log(res);
+        });
+
+        // navigate(location?.state ? location?.state : "/");
       })
       .catch((error) => console.log(error));
   };
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,17 +1,18 @@
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
-
+  const location = useLocation();
+  console.log(location.pathname);
   if (loading) {
     return <span className="loading loading-dots loading-lg"></span>;
   }
   if (user?.email) {
     return children;
   }
-  return <Navigate to="/login" replace></Navigate>;
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
